5ceed7e0d80744ec19b5904d4227372250e7437a
IrillK <71892617+IrillK@users.noreply.github.com>
2020-10-05 15:52:55 +0300

    Create task1.dart

---
+++ task1.dart
@@ -0,0 +1,44 @@
+void main() {
+  
+int monthCount = 12;
+  switch (monthCount) {
+    case 0:
+      print("январь");
+      break;
+    case 1:
+      print("февраль");
+      break;
+    case 2:
+      print("март");
+      break;
+    case 3:
+      print("апрель");
+      break;
+    case 4:
+      print("май");
+      break;
+    case 5:
+      print("июнь");
+      break;
+    case 6:
+      print("июль");
+      break;
+    case 7:
+      print("август");
+      break;
+    case 8:
+      print("сентябрь");
+      break;
+    case 9:
+      print("октябрь");
+      break;
+    case 10:
+      print("ноябрь");
+      break;
+    case 11:
+      print("декабрь");
+      break;
+    default:
+      print("Неверный номер месяца");
+    }
+  }
