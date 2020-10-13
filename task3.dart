e31f19c51cac176a0d896af0885ae35cf1a11472
IrillK <71892617+IrillK@users.noreply.github.com>
2020-10-13 16:16:07 +0300

    Update task3.dart

--- task3.dart
+++ task3.dart
@@ -1,14 +1,30 @@
-//функцию из второго задания невозможно модернизировать, так как значение опциональнального параметра должено быть константой
-
 void changeWordOder([String a = "hello world"]){
   a = a.split(' ').reversed.join(' ');
   print(a);
 }
 
+double arithmeticMean([List<double> arr]) {
+  double sum = 0;
+  for (int i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+
+double arithmeticMean2({List<double> arr}) {
+  double sum = 0;
+  for (int i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
 
 void main() {
   changeWordOder();
   
   String a ="hello world";
   changeWordOder(a);
+  
+  print(arithmeticMean([1, 2, 3]));
+  print(arithmeticMean2(arr: [1, 2, 3]));
 } 
