48d5fb13c800912f6b4c532dd341ba5518174a25
IrillK <71892617+IrillK@users.noreply.github.com>
2020-10-16 17:48:53 +0300

    Update task1.dart

--- task1.dart
+++ task1.dart
@@ -1,36 +1,52 @@
-class User {
-  String name;
-  String surname;
-  User(this.name, this.surname);
-  
-  String toString() => "name: ${name}\nsurname: ${surname}";
+
+class Shape  {
+
+final List<double> borders;
+
+Shape(this.borders);
+
 }
 
-class Student extends  User{
+class Trapezoid extends Shape with BorderHelper2{
+
+Trapezoid() : super([8.0, 5.0, 10.0, 5.0]);
+
+}
+ 
+class Country with BorderHelper2{
+
+final List<double> borders;
+
+Country(this.borders);
+
+}
+
+mixin BorderHelper1 on Shape {
+  int get bordersCount => super.borders.length;  
+}
+
+mixin BorderHelper2 {
   
-  int yearOfAdmission; 
+  int bordersCount(List<double> borders){
+    return borders.length;
+  }
   
-  int get currentCourse{
-    return DateTime.now().year - yearOfAdmission;
-  } 
-  
-  Student(
-    String name,
-    String surname,
-    this.yearOfAdmission
-  ) :super(name, surname);
-  
-  @override
-  String toString(){
-     return super.toString() + "\nyearOfAdmission: ${yearOfAdmission}\ncurrentCourse: ${currentCourse}";
-  } 
 }
 
 void main() {
- User u1 = User("Kirill", "Ivanov");
- print(u1.toString());
+ //сделал два вида миксинов
+  //1 - ограничил миксин для Shape
+  //Trapezoid t1 = Trapezoid();
+  //print(t1.bordersCount);
+  //Но тогда нельзя использовать этот миксин для Country
   
- Student st1 = Student("Kirill", "Ivanov", 2018);
+  //2 - сделал в миксине обращение к полю класса, тогда можно использовать с любым классом
   
- print(st1.toString());
+  
+  Trapezoid t1 = Trapezoid();
+  print(t1.bordersCount(t1.borders));
+  
+  Country c1 = Country([8.0, 5.0, 10.0, 5.0, 1.2]);
+  print(c1.bordersCount(c1.borders));
+  
 } 
