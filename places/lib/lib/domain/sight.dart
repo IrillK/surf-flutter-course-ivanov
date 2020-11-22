e423de32278c3256070206e1050c6521cc209a78
IrillK <71892617+IrillK@users.noreply.github.com>
2020-11-22 21:38:07 +0300

    Delete sight.dart

--- places/lib/lib/domain/sight.dart
+++
@@ -1,9 +0,0 @@
-class Sight{
-  String nameSights;
-  double lat;
-  double lon;
-  String url;
-  String details;
-  String type;
-  Sight(this.nameSights,this.lat,this.lon,this.url,this.details,this.type);
-}
