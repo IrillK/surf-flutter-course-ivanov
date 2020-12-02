e5cbf420aa132cf385c92b2cdc349b53a90770dd
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-02 20:56:36 +0300

    Add files via upload

--- places/lib/main.dart
+++ places/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SightDetails(sight: mocks[0],),
+      home: SightList(),
     );
   }
 }
@@ -130,4 +130,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-//
