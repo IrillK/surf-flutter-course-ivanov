fcfbd107e3cb965a1b364fb894e777c7adcc0dfb
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-02 21:12:42 +0300

    Add files via upload

---
+++ places/lib/domain/sight.dart
@@ -0,0 +1,9 @@
+class Sight{
+  String nameSights;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  String type;
+  Sight(this.nameSights,this.lat,this.lon,this.url,this.details,this.type);
+}
