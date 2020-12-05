84ac7732964930efc59f3e4e72b6d7a490e3acca
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-05 18:39:07 +0300

    Add files via upload

--- places/lib/ui/screen/MyAppBar.dart
+++ places/lib/ui/screen/MyAppBar.dart
@@ -24,55 +24,10 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
           ),
         ),
       ),
-      toolbarHeight: 100,
+      toolbarHeight: preferredSize.height,
       centerTitle: false,
       elevation: 0,
       backgroundColor: Colors.white,
-    );
-  }
-
-  @override
-  Size get preferredSize => Size.fromHeight(height);
-
-
-}
-
-class MyCustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final double height;
-
-  const MyCustomAppBar({
-    Key key,
-    @required this.height,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          color: Colors.grey[300],
-          child: Padding(
-            padding: EdgeInsets.all(30),
-            child: AppBar(
-              title: Container(
-                color: Colors.white,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    contentPadding: EdgeInsets.all(10),
-                  ),
-                ),
-              ),
-              actions: [
-                IconButton(
-                  icon: Icon(Icons.verified_user),
-                  onPressed: () => null,
-                ),
-              ],
-            ) ,
-          ),
-        ),
-      ],
     );
   }
 
