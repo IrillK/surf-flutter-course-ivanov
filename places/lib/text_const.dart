7a9d5d8a62477d12f46565464c5f06ac11eaa56c
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-07 20:05:07 +0300

    Add files via upload

---
+++ places/lib/text_const.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+
+//sight_card
+Color sightCardColor = Color(0xFFF5F5F5);
+double nameSightsFontSize = 16;
+Color nameSightsColor = Color(0xFF3B3E5B);
+
+String shortDescription = "краткое описание";
+double shortDescriptionFontSize = 14;
+Color shortDescriptionColor = Color(0xFF7C7E92);
+
+//sight_list
+String listOfInterestingPlaces = "Список \nинтересных мест";
+FontWeight nameSightsFontWeight = FontWeight.bold;
+double nameSightsListFontSize = 24;
+double typeSightsListFontSize = 14;
+
+String buildRoute = "ПОСТРОИТЬ МАРШРУТ";
+double buildRouteFontSize = 14;
+Color buildRouteColor = Colors.white;
+
+double buttonFontSize = 16;
+FontWeight buttonFontWeight = FontWeight.w400;
+
+String schedule = "Запланировать";
+Color scheduleColor = Color.fromRGBO(124, 126, 146, 0.56);
+
+
+String toFavorites = "В Избранное";
+Color toFavoritesColor = Colors.black;
