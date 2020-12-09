9af3a289a952eda35c037383cbc7fb27ab94a551
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-09 20:04:51 +0300

    Add files via upload

---
+++ places/lib/screen_consts/my_app_bar_consts.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+String listOfInterestingPlaces = "Список \nинтересных мест";
+Color appBarTextColor = Colors.black;
+double appBarFontSize = 32;
+Color appBarBackgroundColor = Colors.white;
