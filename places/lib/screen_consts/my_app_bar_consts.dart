62628056d256ef6080239289717c4d675bc213c2
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-09 20:08:35 +0300

    Delete my_app_bar_consts.dart

--- places/lib/screen_consts/my_app_bar_consts.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:ui';
-import 'package:flutter/material.dart';
-
-String listOfInterestingPlaces = "Список \nинтересных мест";
-Color appBarTextColor = Colors.black;
-double appBarFontSize = 32;
-Color appBarBackgroundColor = Colors.white;
