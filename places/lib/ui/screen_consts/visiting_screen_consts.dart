cba4e9621f037bba25a09b9641821465c8e1477d
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-11 19:44:11 +0300

    Delete visiting_screen_consts.dart

--- places/lib/ui/screen_consts/visiting_screen_consts.dart
+++
@@ -1,4 +0,0 @@
-String wantToVisitVisit = "Хочу посетить/Посещенные места";
-String wantToVisit = "Хочу посетить";
-String visitPlace = "Посещенные места";
-String listOfInterestingPlaces = "Список интересных мест";
