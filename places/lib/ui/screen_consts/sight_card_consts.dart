51b0b80f54ac23eda4a8f1c20206b3233e08df36
IrillK <71892617+IrillK@users.noreply.github.com>
2020-12-11 19:01:35 +0300

    Add files via upload

---
+++ places/lib/ui/screen_consts/sight_card_consts.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+String favoriteBorder = "res/image/favorite_border.png";
+Color typeSightCardColor = Colors.white;
+double typeSightCardFontSize = 14;
+Color sightCardBackgroundColor = Color(0xFFF5F5F5);
+double nameSightsCardFontSize = 16;
+Color nameSightsCardColor = Color(0xFF3B3E5B);
+String shortDescriptionSightCard = "краткое описание";
+double shortDescriptionSightCardFontSize = 14;
+Color shortDescriptionSightCardColor = Color(0xFF7C7E92);
---
+++ places/lib/ui/screen_consts/visiting_screen_consts.dart
@@ -0,0 +1,4 @@
+String wantToVisitVisit = "Хочу посетить/Посещенные места";
+String wantToVisit = "Хочу посетить";
+String visitPlace = "Посещенные места";
+String listOfInterestingPlaces = "Список интересных мест";
